7cb6c71ef3f51c41db2c52c754347268faafcfd5
Ragnarstefanss <ragnars15@ru.is>
2017-02-17 20:29:01 +0000

    Users list with admin showed

--- client/src/app/chat.service.ts
+++ client/src/app/chat.service.ts
@@ -1,208 +1,131 @@
-import { Injectable } from '@angular/core';
+import {
+  Injectable
+} from '@angular/core';
 import * as io from "socket.io-client";
-import {Observable} from "rxjs/Observable";
+import {
+  Observable
+} from "rxjs/Observable";
 
 @Injectable()
 export class ChatService {
-  socket : any;
+  socket: any;
   bleh: any;
   constructor() {
-          this.socket = io("http://localhost:8080/");
-      this.socket.on("connect", function(){
-        console.log("connect");
-      });
-   }
-   login(userName : string) : Observable<boolean>{
-     let observable = new Observable(observer => {
-       this.socket.emit("adduser", userName, succeeded =>{
+    this.socket = io("http://localhost:8080/");
+    this.socket.on("connect", function() {
+      console.log("connect");
+    });
+  }
+  login(userName: string): Observable < boolean > {
+    let observable = new Observable(observer => {
+      this.socket.emit("adduser", userName, succeeded => {
         console.log("Reply receved");
         observer.next(succeeded);
       });
-     });
+    });
 
     return observable;
-   }
-   getRoomsList() : Observable<string[]> {
+  }
+  getRoomsList(): Observable < string[] > {
     let obs = new Observable(observer => {
       this.socket.emit("rooms");
       this.socket.on("roomlist", (lst) => {
         let strArr: string[] = [];
-        for(var x in lst){
+        for (var x in lst) {
           strArr.push(x);
-          console.log((lst).lobby.topic);
-          console.log((lst[x]));
-          console.log((lst[x]).topic);
         }
         observer.next(strArr);
       })
     });
     return obs;
-   }
-/*
-  joinRoom(room : string) : Observable<boolean>{
-     let observable = new Observable(observer => {
-       this.socket.emit("adduser", room, succeeded =>{
-        console.log("Reply receved");
-        observer.next(succeeded);
-      });
-     });
+  }
 
-    return observable;
-   }
-*/
-   joinRoom(roomName: string) : Observable<boolean>{
-        const observable = new Observable(observer => {
-        //todo: VALIDATE THAT THE ROOM NAME IS VALIDATE
-        var param =  {
-          room: roomName
-        }
-        this.socket.emit("joinroom", param, function(a : boolean, b) {
-          observer.next(a);
-        });
+  joinRoom(roomName: string): Observable < boolean > {
+    const observable = new Observable(observer => {
+      //todo: VALIDATE THAT THE ROOM NAME IS VALIDATE
+      var param = {
+        room: roomName
+      }
+      this.socket.emit("joinroom", param, function(a: boolean, b) {
+        observer.next(a);
       });
-      return observable;
-    }
-    showChat(roomName: string) : Observable<string[]> {
+    });
+    return observable;
+  }
+  showChat(roomName: string): Observable < string[] > {
     let obs = new Observable(observer => {
       this.socket.emit("rooms");
       this.socket.on("roomlist", (lst) => {
         let strArr: string[] = [];
 
-        for(var x in lst){
-          if(x == roomName)
-          {
-            for(var y in (lst[x]["messageHistory"]))
-            {
-              console.log("y :"+ y);
-               var tempMsg =(lst[x]["messageHistory"][y]["nick"])+": "+(lst[x]["messageHistory"][y]["message"])+"  "+(lst[x]["messageHistory"][y]["timestamp"]);
-               //var tempMsg =(lst[x]["messageHistory"][y]);
-        console.log(tempMsg);
-          strArr.push(tempMsg);
+        for (var x in lst) {
+          if (x == roomName) {
+            for (var y in (lst[x]["messageHistory"])) {
+              var tempMsg = (lst[x]["messageHistory"][y]["nick"]) + ": " + (lst[x]["messageHistory"][y]["message"]) + "  " + (lst[x]["messageHistory"][y]["timestamp"]);
+              strArr.push(tempMsg);
             }
-          }
-        //console.log((lst).lobby.topic);
-          //console.log((lst[x]));
-          //console.log((lst[x]).topic);
+          };
         }
         observer.next(strArr);
       })
     });
     return obs;
-   }
-
-   showUsers(roomName: string) : Observable<string[]> {
-    let obs = new Observable(observer => {
-
-    //this.socket.on('updatechat',(roomname,lst) => {
-      this.socket.on('updateusers', (roomName, users, ops) =>{
-        let strArr: string[] = [];
-          for(var x in users){
-              strArr.push(x);
-          }
-          observer.next(strArr);
-      })
-
-    });
-    return obs;
-      /*
-      this.socket.emit("rooms");
-      this.socket.on("roomlist", (lst) => {
-        let strArr: string[] = [];
-
-        for(var x in lst){
-          if(x == roomName)
-          {
-            for(var y in (lst[x]["users"]))
-            {
-              console.log("y :"+ y);
-
-               //var tempMsg =(lst[x]["messageHistory"][y]);
-        console.log((lst[x]["users"][y]));
-          strArr.push((lst[x]["users"][y]));
-            }
-          }
-        //console.log((lst).lobby.topic);
-          //console.log((lst[x]));
-          //console.log((lst[x]).topic);
-        }
-        observer.next(strArr);
-      })
-    });
-    return obs;
-    */
-   }
-
-   sendmsg(roomname: string, Msg: string) : Observable<string[]>{
-      const observable = new Observable(observer => {
-        //todo: VALIDATE THAT THE ROOM NAME IS VALIDATE
-        var param =  {
-          roomName: roomname,
-          msg: Msg
-        }
-        this.socket.emit("sendmsg", param);
-
-        this.socket.on('updatechat',(roomname,lst) => {
-          console.log("updatechat");
-          console.log(lst);
-          let strArr: string[] = [];
-          for(var x in lst){
-            var tempMsg =(lst[x]["nick"])+": "+(lst[x]["message"])+"  "+(lst[x]["timestamp"]);
-            console.log(tempMsg);
-          strArr.push(tempMsg);
-        }
-        observer.next(strArr);
-        })
-        /*
-        this.socket.emit("sendmsg", param, function(a : boolean, b){
-          observer.next(a);
-
-        });
-        */
-        //io.sockets.emit('updatechat', data.roomName, rooms[data.roomName].messageHistory);
-
-   });
-   console.log(observable);
-   return observable;
   }
 
-  Leave(room:string) : Observable<boolean>{
-     const observable = new Observable(observer => {
-            var mess = "left the room";
-       var param =  {
-          roomName: room,
-          msg: mess
+  showUsers(roomName: string): Observable < string[] > {
+    let obs = new Observable(observer => {
+      this.socket.on('updateusers', (roomName, users, ops) => {
+        let strArr: string[] = [];
+        for (var x in users) {
+          var temp = x + " *OP*";
+          strArr.push(temp);
         }
-        this.socket.emit("sendmsg", param);
-       this.socket.emit('partroom',room, function(a : boolean, b) {
-          observer.next(a);
-        });
+        for (var x in users) {
+          strArr.push(x);
+        }
+        observer.next(strArr);
+      })
 
+    });
+    return obs;
+  }
 
+  sendmsg(roomname: string, Msg: string): Observable < string[] > {
+    const observable = new Observable(observer => {
+      //todo: VALIDATE THAT THE ROOM NAME IS VALIDATE
+      var param = {
+        roomName: roomname,
+        msg: Msg
+      }
+      this.socket.emit("sendmsg", param);
 
-
-     });
-    //		io.sockets.emit('updateusers', room, rooms[room].users, rooms[room].ops);
-		//io.sockets.emit('servermessage', "part", room, socket.username);
+      this.socket.on('updatechat', (roomname, lst) => {
+        let strArr: string[] = [];
+        for (var x in lst) {
+          var tempMsg = (lst[x]["nick"]) + ": " + (lst[x]["message"]) + "  " + (lst[x]["timestamp"]);
+          strArr.push(tempMsg);
+        }
+        observer.next(strArr);
+      })
+    });
     return observable;
   }
 
-  /*
-joinRoom(roomName: string) : Observable<boolean>{
-        const observable = new Observable(observer => {
-        //todo: VALIDATE THAT THE ROOM NAME IS VALIDATE
-        var param =  {
-          room: roomName
-        }
-        this.socket.emit("joinroom", param, function(a : boolean, b) {
-          observer.next(a);
-        });
+  Leave(room: string): Observable < boolean > {
+    const observable = new Observable(observer => {
+      var mess = "left the room";
+      var param = {
+        roomName: room,
+        msg: mess
+      }
+      this.socket.emit("sendmsg", param);
+      this.socket.emit('partroom', room, function(a: boolean, b) {
+        observer.next(a);
       });
-      return observable;
-    }
-  */
+    });
 
-
-
+    return observable;
+  }
   //partroom
 
 }
--- client/src/app/room/room.component.ts
+++ client/src/app/room/room.component.ts
@@ -8,59 +8,39 @@ import { Router, ActivatedRoute } from "@angular/router";
   styleUrls: ['./room.component.css']
 })
 export class RoomComponent implements OnInit {
+  constructor(private chatService: ChatService, private router: Router, private route: ActivatedRoute) {}
 
+    msg: string;
+    roomId: string;
+    users: string[];
+    messageHistory: string[];
+    topic: string;
 
-  constructor(private chatService : ChatService,private router: Router,private route : ActivatedRoute) { }
-
-  msg: string;
-roomId : string;
-users : string[];
-messageHistory : string[];
-topic: string;
-
-  ngOnInit() {
-    this.roomId = this.route.snapshot.params['id'];
-    /*
-    this.chatService.showChat(this.roomId).subscribe(lst => {
-      console.log("lst :"+lst);
+    ngOnInit() {
+      this.roomId = this.route.snapshot.params['id'];
+      this.chatService.sendmsg(this.roomId, "Joined").subscribe(lst => {
       this.messageHistory = lst;
-    })
-    */
-    this.chatService.sendmsg(this.roomId,"Joined").subscribe(lst =>{
-        //console.log("succeeded?" + succeeded);
-        this.messageHistory = lst;
-      });
+    });
     this.chatService.showUsers(this.roomId).subscribe(lst => {
-      console.log("lst :"+lst);
       this.users = lst;
     })
 
   }
   onSend() {
-      console.log("sendmsg called in component");
-      if(this.msg.length < 1)
-      {
-        return;
-      }
-      this.chatService.sendmsg(this.roomId,this.msg).subscribe(lst =>{
-        //console.log("succeeded?" + succeeded);
-        this.messageHistory = lst;
-      });
-
-      console.log("message sent");
+    if (this.msg.length < 1) {
+      return;
+    }
+    this.chatService.sendmsg(this.roomId, this.msg).subscribe(lst => {
+      this.messageHistory = lst;
+    });
+    console.log("message sent");
   }
-  onLeave(){
-    this.chatService.Leave(this.roomId).subscribe(succeeded =>{
-        if(succeeded === true)
-        {
-          console.log("bleh test");
-          this.router.navigate(["./rooms"]);
-        }
-        else
-        {
-          console.log("bleh else");
-        }
-  });
-  this.router.navigate(["./rooms"]);
+  onLeave() {
+    this.chatService.Leave(this.roomId).subscribe(succeeded => {
+      if (succeeded === true) {
+        this.router.navigate(["./rooms"]);
+      }
+    });
+    this.router.navigate(["./rooms"]);
   }
 }
